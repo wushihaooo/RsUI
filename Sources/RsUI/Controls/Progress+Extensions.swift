72ea881a528f745ed8fae32375debc4626dd93e1
rayman-zhao <zhao_yu@me.com>
2026-04-01 10:15:37 +0800

    Add progressBar with observation

--- Sources/RsUI/Controls/Progress+Extensions.swift
+++ Sources/RsUI/Controls/Progress+Extensions.swift
@@ -2,11 +2,11 @@ import Foundation
 import Observation
 import WinUI
 
-/// NOTE: Must inherit ProgressRing, otherwise can't be triggered the second change of the observation
+/// NOTE: Must inherit ProgressRing/Bar, otherwise can't be triggered the second change of the observation
 public class ProgressRingEx: ProgressRing {
 }
 
-public extension ProgressRing {
+public extension ProgressRingEx {
        func startObserving<Element>(_ emit: @escaping @Sendable () -> Element, onChanged: @escaping @MainActor (ProgressRing, Element) -> Void) {
         let obs = Observations(emit)
 
@@ -15,6 +15,25 @@ public extension ProgressRing {
                 /// NOTE: self is nil after the first change of the observation.
                 /// Does the ring pointer released?
                 /// Why NavigationViewItem works in this case?
+                guard let self else { return }
+                await MainActor.run { [weak self] in
+                    guard let self else { return }
+                    onChanged(self, value)
+                }
+            }
+        }
+    }
+}
+
+public class ProgressBarEx: ProgressBar {
+}
+
+public extension ProgressBarEx {
+       func startObserving<Element>(_ emit: @escaping @Sendable () -> Element, onChanged: @escaping @MainActor (ProgressBar, Element) -> Void) {
+        let obs = Observations(emit)
+
+        Task { [weak self] in
+            for await value in obs {
                 guard let self else { return }
                 await MainActor.run { [weak self] in
                     guard let self else { return }
